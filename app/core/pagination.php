2316724b019e655efdac7cf838e9d345da88e216
Eugene Sherlaimov <sherlaimov@gmail.com>
2015-10-08 10:22:08 +0300

    minor sql fiddling

--- app/core/pagination.php
+++ app/core/pagination.php
@@ -46,10 +46,11 @@ class Pagination extends Model_News {
 
     public function findByOffset(){
 //        return $this->query("SELECT * FROM news ORDER BY id DESC LIMIT $this->per_page OFFSET {$this->offset()}");
-        return $this->query('SELECT n.user_id, n.title, n.body, n.created, u.first_name, u.last_name
-        FROM news AS n
-        INNER JOIN  user AS u ON n.user_id = u.user_id
-        WHERE 1 ORDER BY n.id DESC LIMIT ' . $this->per_page . ' OFFSET ' . $this->offset());
+        return $this->query('SELECT news.id, news.user_id, news.title, news.body, news.created, news.image_name, news.thumb,
+                            user.first_name, user.last_name
+                            FROM news, user
+                            WHERE news.user_id = user.user_id ORDER BY news.id LIMIT '
+         . $this->per_page . ' OFFSET ' . $this->offset());
 
     }
 
--- app/views/news_index.php
+++ app/views/news_index.php
@@ -8,7 +8,7 @@
 <?php
 
 //var_dump($this->user);
-//var_dump($data);
+//var_dump($this->data);
 
 $pagination = $data['pagination'];
 
@@ -56,7 +56,7 @@ $pagination = $data['pagination'];
 foreach ($data['news'] as $news) {
     echo '<div class="row"><div class="col-md-12">';
     echo '<h1>' . '<a href="/news/one_news?article_id=' . $news['id'] . '">' . $news['title'] . '</a>' . '</h1>';
-    echo '<p>By ' . $this->user->fullName() . '</p>';
+    echo '<p>By ' . $news['first_name'] . ' ' . $news['last_name'] . '</p>';
     echo '<p><span class="glyphicon glyphicon-time"></span> Posted on ' . $news['created'] . '</p></div><hr>';
     echo '<div class="col-md-4"><img src="' . WS_IMAGES . 'thumb/' . $news['thumb'] . '" class="news-image"></div>';
     echo '<div class="col-md-8"><p>' . $news['body'] . '</p></div>';
